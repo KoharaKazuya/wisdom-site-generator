c4e9a92c221214451a6a28bdd5f851252a3d4d67
KoharaKazuya <reflect.results@gmail.com>
2020-10-11 01:23:59 +0900

    feat: update wisdom-http-api headers

--- server.js
+++ server.js
@@ -10,19 +10,27 @@ const fetch = require("node-fetch");
 const express = require("express");
 const next = require("next");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const chalk = require("chalk");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const proxyTarget = process.env.PROXY_TARGET || "http://localhost:3001";
-const proxyDebugCognitoUsername = process.env.PROXY_DEBUG_COGNITO_USERNAME;
+const proxyDebugUserName = process.env.PROXY_DEBUG_USER_NAME;
+const proxyDebugUserEmail = process.env.PROXY_DEBUG_USER_EMAIL;
 
 fetch(proxyTarget, { method: "HEAD" }).catch(() => {
   console.warn(
-    "/api 以下を利用するには wisdom-http-api を起動し、環境変数 PROXY_TARGET にそのアドレスを指定する必要があります"
+    chalk.bold.yellow.inverse(" WARN "),
+    chalk.yellow(
+      "/api 以下を利用するには wisdom-http-api を起動し、環境変数 PROXY_TARGET にそのアドレスを指定する必要があります"
+    )
   );
 });
-if (!proxyDebugCognitoUsername)
+if (!(proxyDebugUserName && proxyDebugUserEmail))
   console.warn(
-    "/api 以下を利用するには環境変数 PROXY_DEBUG_COGNITO_USERNAME にデバッグ用 Cognito ユーザーの username を指定する必要があります"
+    chalk.bold.yellow.inverse(" WARN "),
+    chalk.yellow(
+      "/api 以下を利用するには環境変数 PROXY_DEBUG_USER_NAME と PROXY_DEBUG_USER_EMAIL にデバッグ用ユーザー情報を指定する必要があります"
+    )
   );
 
 const app = next({ dev: true });
@@ -40,10 +48,8 @@ app.prepare().then(() => {
         "^/api/[^/]+": "",
       },
       onProxyReq(proxyReq, req, res) {
-        proxyReq.setHeader(
-          "wisdom-cognito-username",
-          proxyDebugCognitoUsername
-        );
+        proxyReq.setHeader("wisdom-user-name", encodeURI(proxyDebugUserName));
+        proxyReq.setHeader("wisdom-user-email", encodeURI(proxyDebugUserEmail));
       },
       logLevel: "warn",
     })
